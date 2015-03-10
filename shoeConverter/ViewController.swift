30279fbb6295316e1710e7869e35c639bd03ba49
terryAtkins <git2hubs@gmail.com>
2015-03-10 09:36:27 +0000

    add shoeConverter files

--- shoeConverter/ViewController.swift
+++ shoeConverter/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    
+    @IBOutlet weak var womensShoeSizeTextFeild: UITextField!
+    
+    @IBOutlet weak var womensShoeSizeLabel: UILabel!
+    @IBOutlet weak var showConvertSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeConvertLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        let shoeSizeFromTextField = mensShoeSizeTextField.text.toInt()!
+        let conversionConstant = 30
+        
+        mensConvertedShoeSizeLabel.hidden = false
+        showConvertSizeLabel.hidden = false
+        
+        mensConvertedShoeSizeLabel.text = "\(shoeSizeFromTextField + conversionConstant)"
+        mensShoeSizeTextField.text = ""
+        mensShoeSizeTextField.resignFirstResponder()
+        
+    }
+    @IBAction func womensConvertButtonPressed(sender: UIButton) {
+        let shoeSizeFromTextField = Double((womensShoeSizeTextFeild.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        
+        womensShoeSizeLabel.hidden = false
+        womensShoeSizeConvertLabel.hidden = false
+        
+        womensShoeSizeLabel.text = "\(shoeSizeFromTextField + conversionConstant)"
+        womensShoeSizeTextFeild.text = ""
+        womensShoeSizeTextFeild.resignFirstResponder()
+    }
 
 }
 
